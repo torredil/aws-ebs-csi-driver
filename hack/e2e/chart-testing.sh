662cbf898e74ad016f4b1d76cf5e9654131b70aa
Eddie Torres <torredil@amazon.com>
2022-11-22 14:31:01 +0000

    Add test-helm-chart target to Makefile to lint/test Helm chart upgrades
    Signed-off-by: Eddie Torres <torredil@amazon.com>

---
+++ hack/e2e/chart-testing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -uo pipefail
+
+function ct_install() {
+  INSTALL_PATH=${1}
+  CHART_TESTING_VERSION=${2}
+  if [[ ! -e ${INSTALL_PATH}/chart-testing ]]; then
+    CHART_TESTING_DOWNLOAD_URL="https://github.com/helm/chart-testing/releases/download/v${CHART_TESTING_VERSION}/chart-testing_${CHART_TESTING_VERSION}_linux_amd64.tar.gz"
+    curl --silent --location "${CHART_TESTING_DOWNLOAD_URL}" | tar xz -C "${INSTALL_PATH}"
+    chmod +x "${INSTALL_PATH}"/ct
+  fi
+
+  export PATH=/root/.local/bin:$PATH
+  python3 -m pip install --upgrade pip
+  python3 -m pip install --user yamllint yamale
+}
